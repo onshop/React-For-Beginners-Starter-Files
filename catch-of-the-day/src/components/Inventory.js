ccf2cf72a74a2aa48d0cc6297867cf6aa9eb25be
Ben Parish <ben.parish@onshop.co.uk>
2020-01-01 17:52:54 +0000

    Screen 4

---
+++ catch-of-the-day/src/components/Inventory.js
@@ -0,0 +1,13 @@
+import React, {Fragment} from 'react';
+
+class Inventory extends React.Component {
+    render() {
+        return (
+            <div className="inventory">
+                Inventory!!
+            </div>
+        )
+    }
+}
+
+export default Inventory;
